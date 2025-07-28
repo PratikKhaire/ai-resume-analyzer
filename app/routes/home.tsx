3560c5dfdaca34caad4f22dd369ce08ccb973b4f
Pratik Khaire <Pratik.shekhar.khaire@gmail.com>
2025-07-28 15:36:16 +0530

    done with hero section :

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,12 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import { callbackify } from "util";
+import ResumeCard from "~/components/ResumeCard";
+
+import { resumes } from "../../constants/index"; // <-- Update import path
 // import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "Resumind" },
     { name: "description", content: "Smart feedback for your dream job " },
@@ -10,16 +14,22 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main>
-    <Navbar/>
-    <section className="bg-[url('/images/bg-main.svg')] bg-cover">
-      <div className="main-section">
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    {/* {window.puter.ai.chat()}  */}
+    <section className=" main-section">
+      <div className="page-heading  py-16">
         <h1>Track Your Application & Resume Ratings </h1>
         <h2>Review your submission and check AI-powered feedback.</h2>
       </div>
 
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+    )}
     </section>
-
-    {[]}
   </main>
 }
